65530af479ebed1e3a488d8a0be3c655f894915b
Yana <petrova-ys@yandex.ru>
2024-10-29 22:23:38 +0300

    Diplom

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ import data
 def test_get_order_info_by_track():
     track = request.create_order(data.order_body).json()['track']
     response = request.get_order_info_by_track(track)
-    assert response.status_code == 200
+    assert response.status_cde == 200
